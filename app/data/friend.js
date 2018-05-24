2a945cd168160ebb381577a640c7cbf0ac6fb1dc
laurawilsone <laura.wils204@gmail.com>
2018-05-24 12:15:09 -0500

    public and data

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,14 +1,20 @@
-
-// friends data
-
 var friendList = [
     {
-        name: "",
-        profilePic: "",
-        scores: [
-
-        ]
+      name: 'Laura Wilson',
+      profilePic: 'https://avatars3.githubusercontent.com/u/35203224?s=460&v=4',
+      scores: [
+        "5",
+        "4",
+        "3",
+        "5",
+        "2",
+        "4",
+        "3",
+        "4",
+        "3",
+        "3"
+      ]
     }
-]
-
-module.exports = friendList;
+  ]
+  
+  module.exports = friendList;
